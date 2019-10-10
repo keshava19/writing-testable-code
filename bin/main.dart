0c3fdcd52b95de0f722a000c33dd226868b8a557
Keshava Muraari <keshava@debian.eagle>
2019-10-10 12:59:39 +0530

    Initial Commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:writing_testable_code/writing_testable_code.dart' as writing_testable_code;
+
+main(List<String> arguments) {
+  print('Hello world: ${writing_testable_code.calculate()}!');
+}
---
+++ test/writing_testable_code_test.dart
@@ -0,0 +1,8 @@
+import 'package:writing_testable_code/writing_testable_code.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
